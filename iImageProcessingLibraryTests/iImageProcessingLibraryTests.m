65ebeae9cbf271b88eca2ff1091db30f68b7aa7a
Yongseok Kang <feelform@gmail.com>
2011-10-27 23:28:02 +0900

    clear fail test.

--- iImageProcessingLibraryTests/iImageProcessingLibraryTests.m
+++ iImageProcessingLibraryTests/iImageProcessingLibraryTests.m
@@ -10,23 +10,5 @@
 
 @implementation iImageProcessingLibraryTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in iImageProcessingLibraryTests");
-}
 
 @end
